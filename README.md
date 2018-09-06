e6ac3805a1156d2bd2498f2be599d3316e43c29e
Konner Robbins <klrobbins0054@eagle.fgcu.edu>
2018-09-06 09:41:31 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Game-of-some-type--konners-
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//Konner Robbins was here
+//a sweet program that I do not know what to do with
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Goodbye cruel World");
+		
+	}
+
+}
